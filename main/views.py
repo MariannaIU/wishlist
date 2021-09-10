5511f8457a019b14b04919700284c457a6a642c8
Marianna <marianka101@gmail.com>
2021-09-10 19:17:26 +0200

    form added

--- main/views.py
+++ main/views.py
@@ -1,26 +1,36 @@
 from django.shortcuts import render, get_object_or_404
+from datetime import datetime
+
 from .models import WishList
+from .forms import ProductForm
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, 'index.html', {"title": "Wishlist | home page"})
 
 def about(request):
     return render(request, 'about.html', {"title": "Wishlist | about project"})
 
 def list_page(request, pk):
-    """
-    FBV - function based view
-    CBV - class based view
-    """
+    """View page the wishlist"""
     wishlist = get_object_or_404(WishList, pk=pk)
-    print('[wishlist]', wishlist)
+
+    if request.method=='POST':
+        form = ProductForm(request.POST)
+        instance_product = form.save()
+        wishlist.product.add(instance_product)
+        wishlist.save()
+
+    elif request.method=='GET':
+        form = ProductForm()
+
     return render(
         request,
         "wish_list.html",
         {
             'wishlist':wishlist,
             'is_owner_list': wishlist.owner == request.user,
+            'form': form,
         }
     )
 
